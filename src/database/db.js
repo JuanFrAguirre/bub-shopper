a4a9a0fc3ae65a31f483704c037dfc49609bee9d
Juan Francisco Aguirre <jf_aguirre@hotmail.com>
2023-02-25 05:03:12 +0100

    trying new db vars

--- src/database/db.js
+++ src/database/db.js
@@ -1,14 +1,5 @@
 import { Sequelize } from 'sequelize'
 
-const sequelize = new Sequelize(
-  process.env.MYSQLDATABASE,
-  process.env.MYSQLUSER,
-  process.env.MYSQLPASSWORD,
-  {
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
-    dialect: 'mysql',
-  },
-)
+const sequelize = new Sequelize(process.env.MYSQL_URL)
 
 export default sequelize
