879c000c6b4e460ffe2b38970c3991889f6e1af3
Juan Francisco Aguirre <jf_aguirre@hotmail.com>
2023-02-25 05:04:25 +0100

    adding explicit dialect

--- src/database/db.js
+++ src/database/db.js
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize'
 
-const sequelize = new Sequelize(process.env.MYSQL_URL)
+const sequelize = new Sequelize(process.env.MYSQL_URL, { dialect: 'mysql' })
 
 export default sequelize
