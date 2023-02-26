18ceb9233a8d393ec6ba537885adaaf85b18ab7c
Juan Francisco Aguirre <jf_aguirre@hotmail.com>
2023-02-26 02:27:40 +0100

    added cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
 import morgan from 'morgan'
+import cors from 'cors'
 dotenv.config()
 
 const app = express()
@@ -8,5 +9,6 @@ const app = express()
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cors())
 
 export default app
